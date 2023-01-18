e95652de46e805d26fee5f0c4b49e1084fe84033
dosantosclaudio <cdosantos@it10coop.com.ar>
2023-01-17 22:03:49 -0300

    add mutants generation with Major

--- run-all.sh
+++ run-all.sh
@@ -1,15 +1,46 @@
 # ./run.sh relative_path class method package
 
-./run.sh Angle_getTurn/ Angle getTurn\(double,double\) jts
-./run.sh ArithmeticUtils_subAndCheck/ ArithmeticUtilsNew subAndCheck\(long,long\) org.apache.commons.math3.util
-./run.sh BooleanUtils_compare/ BooleanUtils compare\(boolean,boolean\) lang3
-./run.sh BooleanUtils_toBoolean/ BooleanUtils toBoolean\(Integer,Integer,Integer\) lang3
-
+# ./run.sh Angle_getTurn/ Angle getTurn\(double,double\) jts
+# ./run.sh ArithmeticUtils_subAndCheck/ ArithmeticUtilsNew subAndCheck\(long,long\) org.apache.commons.math3.util
+# ./run.sh BooleanUtils_compare/ BooleanUtils compare\(boolean,boolean\) lang3
+# ./run.sh BooleanUtils_toBoolean/ BooleanUtils toBoolean\(Integer,Integer,Integer\) lang3
+# ./run.sh composite_addChild/ Composite addChild\(Composite\) examples
+# ./run.sh doublylinkedlistnode_insertRight/ DoublyLinkedListNode insertRight\(DoublyLinkedListNode\) examples
+# ./run.sh doublylinkedlistnode_remove/ DoublyLinkedListNode remove\(\) examples
+# ./run.sh Envelope_maxExtent/ Envelope maxExtent\(\) jts
+# ./run.sh FastMathNew_floor/ FastMathNew floor\(double\) org.apache.commons.math3.util
+# ./run.sh IntMath_mod/ IntMath mod\(int,int\) math
+# ./run.sh listcomp02_insert_r/ ListComp02 insert_r\(R\) examples
+# ./run.sh listcomp02_insert_s/ ListComp02 insert_s\(S\) examples
+# ./run.sh map_count/ Map count\(\) examples
+# ./run.sh map_extend/ Map extend\(K,V\) examples
+# ./run.sh map_remove/ Map remove\(K\) examples
+# ./run.sh MathUtil_clamp/ MathUtil clamp\(int,int,int\) jts
+# ./run.sh MathUtilsNew_copySignInt/ MathUtilsNew copySignInt\(int,int\) org.apache.commons.math3.util
+# ./run.sh maxbag_add/ MaxBag add\(Integer\) examples
+# ./run.sh maxbag_getMax/ MaxBag getMax\(\) examples
+# ./run.sh maxbag_remove/ MaxBag remove\(Integer\) examples
+# ./run.sh minFinder_findMin/ MinFinder findMin\(\) examples
+# ./run.sh polyupdate_a1/ Polyupdate a1\(int\) examples
+# ./run.sh polyupdate_sm/ Polyupdate sm\(\) examples
+# ./run.sh QueueAr_dequeue/ QueueAr "dequeue\(\)" DataStructures
+# ./run.sh QueueAr_dequeueAll/ QueueAr dequeueAll\(\) DataStructures
+# ./run.sh QueueAr_enqueue/ QueueAr enqueue\(Object\) DataStructures
+# ./run.sh QueueAr_getFront/ QueueAr getFront\(\) DataStructures
+# ./run.sh QueueAr_makeEmpty/ QueueAr makeEmpty\(\) DataStructures
+# ./run.sh ringbuffer_count/ RingBuffer count\(\) examples
+# ./run.sh ringbuffer_extend/ RingBuffer extend\(G\) examples
+# ./run.sh ringbuffer_item/ RingBuffer item\(\) examples
+# ./run.sh ringbuffer_remove/ RingBuffer remove\(\) examples
+# ./run.sh ringbuffer_wipeOut/ RingBuffer wipeOut\(\) examples
+# ./run.sh SimpleMethods_abs/ SimpleMethods abs\(int\) examples
+# ./run.sh SimpleMethods_addElementToSet/ SimpleMethods addElementToSet\(HashSet\<Integer\>,int\) examples
 ./run.sh SimpleMethods_getMin/ SimpleMethods getMin\(int,int\) examples
-./run.sh SimpleMethods_addElementToSet/ SimpleMethods addElementToSet\(HashSet\<Integer\>,int\) examples
-./run.sh SimpleMethods_abs/ SimpleMethods abs\(int\) examples
-./run.sh SimpleMethods_incrementNumberAtIndex/ SimpleMethods "incrementNumberAtIndex\(int\[\],int\)" examples
-
-./run.sh StackAr_makeEmpty/ StackAr makeEmpty\(\) DataStructures
-./run.sh StackAr_pop/ StackAr pop\(\) DataStructures
-./run.sh StackAr_push/ StackAr push\(Object\) DataStructures
+# ./run.sh SimpleMethods_incrementNumberAtIndex/ SimpleMethods "incrementNumberAtIndex\(int\[\],int\)" examples
+# ./run.sh StackAr_makeEmpty/ StackAr makeEmpty\(\) DataStructures
+# ./run.sh StackAr_pop/ StackAr pop\(\) DataStructures
+# ./run.sh StackAr_push/ StackAr push\(Object\) DataStructures
+# ./run.sh StackAr_top/ StackAr "top\(\)" DataStructures
+# ./run.sh StackAr_topAndPop/ StackAr topAndPop\(\) DataStructures
+# ./run.sh structure_foo/ Structure foo\(\) examples
+# ./run.sh structure_setX/ Structure setX\(int\) examples
--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 
 # ---------- settings --------
 run_randoop=true
-run_daikon=true
+run_daikon=false
 curr_dir=$(pwd)
 junit=$curr_dir/lib/junit-4.13.2.jar
 daikon_path=$DAIKONDIR
@@ -20,19 +20,21 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 # -----------------
 
+rm -rf $output_dir
+
 subject_jar=${dir}build/libs/simple-examples-1.0-SNAPSHOT.jar
 package=$4
 class=${package}.$2 # "examples.SimpleMethods" 
 method=${class}.$3 # "examples.SimpleMethods.getMin\(int,int\)"
 class_path=${package//[\.]/\/} # examples/SimpleMethods
 method_without_args=${3%%\(*} # getMin
+method_without_args=${method_without_args%%\\*} # getMin fix '\'
 
 tests_src=$evospex/tests/
 tests_bin=$evospex/tests/bin/
 
 mkdir -p ${output_dir}daikon
 mkdir -p ${output_dir}randoop
-mkdir -p ${dir}test/daikon/
 
 # RANDOOP
 if [ "$run_randoop" = true ] ; then
@@ -54,17 +56,17 @@ fi
 tests_bin=${output_dir}randoop/bin/
 if [ "$run_daikon" = true ] ; then
   java -cp ${tests_bin}:${daikon_path}/daikon.jar:${subject_jar} daikon.DynComp \
-    --output-dir=${dir}test/daikon/ \
+    --output-dir=${output_dir}daikon/ \
     ${package}.RegressionTestDriver
 
   java -cp ${tests_bin}:${daikon_path}/daikon.jar:${subject_jar} daikon.Chicory \
-    --comparability-file=${dir}test/daikon/RegressionTestDriver.decls-DynComp \
-    --output-dir=${dir}test/daikon/ \
+    --comparability-file=${output_dir}daikon/RegressionTestDriver.decls-DynComp \
+    --output-dir=${output_dir}daikon/ \
     ${package}.RegressionTestDriver
 
   java -cp ${daikon_path}/daikon.jar daikon.Daikon \
     -o ${output_dir}daikon/res.inv.gz \
-    ${dir}test/daikon/RegressionTestDriver.dtrace.gz
+    ${output_dir}daikon/RegressionTestDriver.dtrace.gz
 
   java -cp ${daikon_path}/daikon.jar daikon.PrintInvariants \
     ${output_dir}daikon/res.inv.gz --ppt-select ${class}':::OBJECT' --format java > ${output_dir}daikon/res.txt
@@ -73,19 +75,23 @@ if [ "$run_daikon" = true ] ; then
     ${output_dir}daikon/res.inv.gz --ppt-select ${class}'.'${method_without_args} --format java >> ${output_dir}daikon/res.txt
 fi
 
+# MAJOR
+mkdir ${output_dir}bin
+echo '> Generating mutants with Major for file: '$2.java
+$MAJOR_HOME/bin/javac -cp $subject_jar -nowarn -J-Dmajor.export.mutants=true -XMutator:ALL -d ${output_dir}bin ${dir}src/main/java/$class_path/${2}.java
 
+echo '> Mutants generated!'
+mv mutants.log ${output_dir}mutants.log
+mv mutants ${output_dir}
 
+echo ''
+echo '> Processing mutants'
+for dir in ${output_dir}mutants/*/
+do
+  echo '> Procesing mutant: '$dir
+  echo '> Compiling mutant'
+  javac -cp ${output_dir}bin -g $dir$class_path/${2}.java -d $dir
+  echo '> Mutant compiled'
+done
 
-
-
-
-
-
-
-
-
-
-
-
-# java -cp ${dir}test/daikon/:${daikon_path}/daikon.jar daikon.tools.jtb.Annotate \
-#   ${dir}test/daikon/res.inv.gz ${dir}src/StackAr.java
+java -cp ${output_dir}mutants/2:${output_dir}randoop/bin:$junit:$HAMCREST_CORE org.junit.runner.JUnitCore examples.RegressionTest0
